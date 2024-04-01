7d8e77e3d4473ac6a801be887ecbd3050ccaac4e
deenpuru <deenpuru@gmail.com>
2024-04-01 23:23:40 +0300

    Jump

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,14 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UIElements;
 
-public class Car : MonoBehaviour
+public class Player : MonoBehaviour
 {
     [SerializeField] private float _moveXSpeed = 1.0f;
     [SerializeField] private float _moveYSpeed = 0.01f;
-    [SerializeField] private float _jumpHeight = 1.0f;
+    [SerializeField] private float _jumpSpeed = 1.0f;
+    [SerializeField] private float _extraJumpSpeed = 1.0f;
     [SerializeField] private float _destroyDelay = 0;
     [SerializeField] private float _turnTiny = 1f;
     [SerializeField] private float _turnNormal = 1f;
@@ -18,21 +20,28 @@ public class Car : MonoBehaviour
     [SerializeField] private Color32 _hasPackageColor = new Color32(1 ,1 ,1 ,1);
     [SerializeField] private Color32 _noPackageColor = new Color32(1, 1, 1, 1);
 
+    [SerializeField] private Sprite _jumpSprite;
+    [SerializeField] private Sprite _standSprite;
+
     private bool _hasPackage;
     private bool _isJumping;
 
 
     void Update()
-    {
-        float _moveX = Input.GetAxis("Horizontal") * _moveXSpeed * Time.deltaTime;
-        float _moveY = Input.GetAxis("Vertical") * _moveYSpeed * Time.deltaTime;
-        transform.Translate(_moveX,0, 0);
-        transform.Translate(0,_moveY, 0);
+    {        
         PlayerMovement();
     }
 
     private void PlayerMovement()
     {
+        // movement
+        float _moveX = Input.GetAxis("Horizontal") * _moveXSpeed * Time.deltaTime;
+        float _moveY = Input.GetAxis("Vertical") * _moveYSpeed * Time.deltaTime;
+        transform.Translate(_moveX, 0, 0);
+        transform.Translate(0, _moveY, 0);
+        
+        // flipping the image
+
         if (Input.GetAxis("Horizontal") >= 0.1)
         {
             gameObject.GetComponent<SpriteRenderer>().flipX = true;
@@ -41,12 +50,23 @@ public class Car : MonoBehaviour
         {
             gameObject.GetComponent<SpriteRenderer>().flipX = false;
         }
+
+        // jumping
         if (Input.GetKeyDown(KeyCode.Space) && !_isJumping)
         {
+            JumpingSprite();
             _isJumping = true;
-            rb.AddForce(new Vector2(rb.velocity.x, _jumpHeight));
-        }
-      
+            rb.velocity = Vector2.up * _jumpSpeed;
+        }  
+    }
+    private void JumpingSprite()
+    {
+        gameObject.GetComponent<SpriteRenderer>().sprite = _jumpSprite;
+    }
+
+    private void StandSprite()
+    {
+        gameObject.GetComponent<SpriteRenderer>().sprite = _standSprite;
     }
 
     private void OnCollisionEnter2D(Collision2D other)
@@ -54,6 +74,7 @@ public class Car : MonoBehaviour
        if (other.gameObject.CompareTag("Ground"))
         {
             _isJumping = false;
+            StandSprite();
         }
     }
 
@@ -76,12 +97,14 @@ public class Car : MonoBehaviour
 
         if (other.tag == ("ExtraJump"))
         {
-            Debug.Log("touched the extra jump cube");
-            if (Input.GetKeyDown(KeyCode.Space))
+            if (Input.GetKey(KeyCode.Space))
             {
+                _isJumping = false;
+
                 Debug.Log("Jump");
-                rb.AddForce(new Vector2(rb.velocity.x, _jumpHeight));
             }
+            
+            //rb.velocity = Vector2.up * _jumpSpeed * _extraJumpSpeed;
         }
 
 
